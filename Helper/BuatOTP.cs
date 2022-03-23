64467d5b72ddcb286b963d405327d7560f02b504
yusfa julian <yusfajulian44@gmail.com>
2022-03-23 14:42:34 +0700

    Add project files.

---
+++ Helper/BuatOTP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traveler.Helper
+{
+    public static class BuatOTP
+    {
+        public static int buatOTP()
+        {
+            Random r = new();
+            return r.Next(1000, 9999);
+        }
+    }
+}
---
+++ Helper/BuatPrimary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traveler.Helper
+{
+    public class BuatPrimary
+    {
+        public static string buatPrimary()
+        {
+            Guid buat = Guid.NewGuid();
+            return buat.ToString();
+        }
+    }
+}
